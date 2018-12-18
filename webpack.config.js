3a1e945cba163620047f81826b524729754b057c
Yusupov Danil <danil.usupov@icl-services.com>
2018-12-18 18:42:29 +0300

    App initialized

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        include: path.resolve(__dirname, './src')
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, './src'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    compress: true,
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './templates/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
