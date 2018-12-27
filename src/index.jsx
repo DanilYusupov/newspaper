1456fbfb618c39d0453893eb855f988d59f01250
Yusupov Danil <danil.usupov@icl-services.com>
2018-12-27 10:09:15 +0300

    Configured and deployed.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import App from './container/App'
 import '../node_modules/semantic-ui-css/semantic.min.css'
 import '../node_modules/react-app-polyfill/ie9'
+import './js/appinit'
 
 render(
     <App/>,
