acfc1297cf79a7b76688f0d66c4cfa5c88751ab9
Yusupov Danil <danil.usupov@icl-services.com>
2018-12-27 13:19:28 +0300

    Configured webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -50,5 +50,31 @@ module.exports = {
   ],
   output: {
     chunkFilename: '[name].[chunkhash].js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    },
+    runtimeChunk: {
+      name: 'runtime'
+    },
+    namedChunks: true,
+    removeAvailableModules: true,
   }
 }
