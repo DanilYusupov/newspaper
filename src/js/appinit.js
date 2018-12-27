94c58409a22211a22986bca577fe630787158afb
Yusupov Danil <danil.usupov@icl-services.com>
2018-12-27 10:16:05 +0300

    Fixed eslint.

--- src/js/appinit.js
+++ src/js/appinit.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyCRqkmBr20s5eP_mRrjSdQvvu13sJZZ0BI",
-  authDomain: "newspaper-379fd.firebaseapp.com",
-  databaseURL: "https://newspaper-379fd.firebaseio.com",
-  storageBucket: "newspaper-379fd.appspot.com",
+  apiKey: 'AIzaSyCRqkmBr20s5eP_mRrjSdQvvu13sJZZ0BI',
+  authDomain: 'newspaper-379fd.firebaseapp.com',
+  databaseURL: 'https://newspaper-379fd.firebaseio.com',
+  storageBucket: 'newspaper-379fd.appspot.com'
 }
 
 firebase.initializeApp(config)
